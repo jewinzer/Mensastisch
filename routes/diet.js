ded9c62d10d932a92df956908cc1e9fc310baf84
jewinzer <jewinzer@gmail.com>
2022-01-07 21:17:35 +0100

    tweaks

--- routes/diet.js
+++ routes/diet.js
@@ -6,6 +6,13 @@ let router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
-
+//find canteen by city
+router.get('/', function(req,res){
+    const list= ['a','b','c'];
+    res.render('diet', {// render diet.ejs
+        title:'',
+        list: list 
+    })
+});
 
 module.exports = router;
