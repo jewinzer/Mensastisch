c0a3578943c1c792fcca7e9b906ae14a5a3b1e94
jewinzer <jewinzer@gmail.com>
2022-01-13 19:31:42 +0100

    add multiple stores to indb

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ var elems = document.querySelectorAll('.dropdown-trigger');
 var instances = M.Dropdown.init(elems, {
     coverTrigger: false,
     hover: true,
-});
+})
 
 //handle fetch request errors
 function handleFetchErrors(response) {
