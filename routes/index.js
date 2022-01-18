33c686c048d5487b7bb401f03dedbab59bac8ec3
jewinzer <jewinzer@gmail.com>
2022-01-18 09:29:51 +0100

    add canteen favourite icon and logic + feedback on user input

--- routes/index.js
+++ routes/index.js
@@ -22,12 +22,15 @@ router.get('', function(req, res) {
                                 '<button class ="btn-flat disabled" id="dateBtn"></button>'+
                                 '<button class="btn-flat" onclick="refreshDateBtn(this.previousElementSibling.innerHTML, true)">'+
                                 '<i class="material-icons">chevron_right</i></button></div>',
+            heroContentSecondary: '<i class="material-icons icon-clickable"'+
+                                `onclick="changeIconState(this); updateFavouriteCanteens(${id})">notifications_none</i>`,
             mainContent: '<ul id="searchResults"></ul>',
             onload: `showCanteenData(${id})`
         });
     }
     return res.render('index', {
         heroContentPrimary: '<h1>Hello from EJS template engine</h1>',
+        heroContentSecondary: '',
         mainContent: 'Index first view',
         onload: 'null'
     })
