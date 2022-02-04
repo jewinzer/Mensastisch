e73d8f062b7982fb2e13125a8bc79f49f077c2e5
SaschaHeinze <saschamheinze@gmail.com>
2022-02-04 09:24:04 +0100

    edited navbar to icons with links

--- routes/index.js
+++ routes/index.js
@@ -27,14 +27,9 @@ router.get('', function(req, res) {
         });
     }
     return res.render('index', {
-<<<<<<< HEAD
-        heroContentPrimary: '<h1>Hello from EJS template engine</h1>',
-        mainContent: '<img class="responsive" src="/img/mensastisch.svg" alt="SVG mit img Tag laden">',
-=======
         heroContentPrimary: '<h1>Startseite</h1>',
         heroContentTertiary: '',
         mainContent: 'Startseiten-Content',
->>>>>>> f5b3376789f6c992965320a2146297226c5af317
         onload: 'null'
     })
 });
