16e12d75e72779f6e8bd165ed54539764c79da41
Ben Eric <benben2010@me.com>
2022-02-08 19:32:10 +0100

    insert calendar

--- routes/calendar.js
+++ routes/calendar.js
@@ -15,7 +15,9 @@ router.get('', function(req, res) {
 
     //render calendar.ejs
     res.render('calendar', {
- 
+        heroContentPrimary: '<h1>Essensplan</h1>',
+        heroContentTertiary: 'Hier siehst du deine ausgewählten Gerichte für die aktulle Woche.',
+        mainContent: 'Essensplan-Content',
     })
 });
 
