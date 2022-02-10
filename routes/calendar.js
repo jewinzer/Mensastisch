c98d1939569b0e58bf6d7f20535555515f7c21b5
jewinzer <jewinzer@gmail.com>
2022-02-10 16:34:28 +0100

    sticky nav and collection headers

--- routes/calendar.js
+++ routes/calendar.js
@@ -16,8 +16,8 @@ router.get('', function(req, res) {
     //render calendar.ejs
     res.render('calendar', {
         heroContentPrimary: '<h1>Essensplan</h1>',
-        heroContentTertiary: 'Hier siehst du deine ausgewählten Gerichte für die aktulle Woche.',
-        mainContent: 'Essensplan-Content',
+        heroContentTertiary: 'Hier siehst du deine ausgewählten Gerichte für die aktuelle Woche.',
+        mainContent: '<p class="center-align">Bisher keine Einträge.<p>',
     })
 });
 
